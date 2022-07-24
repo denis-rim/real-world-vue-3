9f569861ca6bc233548bc5ea469fae9f333623cd
dmazurchuk <dmazurchuk@emphasoft.com>
2022-07-24 14:29:47 +0300

    refactor, add events list and card components, routing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="app">
+    <nav>
+      <router-link to="/">Events</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <style>
@@ -26,5 +28,9 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h1 {
+  font-size: 25px;
 }
 </style>
