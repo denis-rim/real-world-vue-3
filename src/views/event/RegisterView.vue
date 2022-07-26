cc50031d2a4f3f385ea7c3ae59568a490d8d2e3a
Denis Mazurchuk <denis.aurorim@gmail.com>
2022-07-26 14:26:08 +0300

    programmatic navigation

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,10 +1,17 @@
 <template>
   <h1>Register for the event</h1>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
   props: ['event'],
+  methods: {
+    register() {
+      console.log('Registering for the event')
+      this.$router.push({ name: 'EventDetails' })
+    },
+  },
 }
 </script>
 
